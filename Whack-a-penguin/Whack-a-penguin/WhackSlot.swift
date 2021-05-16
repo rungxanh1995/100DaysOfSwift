4cbb360b0ea6c66f9e00a45e4158d2b64db905fc
Joe Pham <rungxanh2901@gmail.com>
2021-05-16 15:31:29 -0400

    Project 14: Challenge 3
    * Added smoke effect when penguins are hit, and mud effect when they emerge or submerge from a slot
    * Some refactoring and acces control edits
    
    Signed-off-by: Joe Pham <rungxanh2901@gmail.com>

--- Whack-a-penguin/Whack-a-penguin/WhackSlot.swift
+++ Whack-a-penguin/Whack-a-penguin/WhackSlot.swift
@@ -11,10 +11,12 @@ class WhackSlot: SKNode {
     private var penguinNode: SKSpriteNode!
     var isVisible = false
     var isHit = false
-	static public let goodPenguinName = "penguinGood"
-	static public let badPenguinName = "penguinBad"
-	static public let whackBadSound	= "whackBad.caf"
-	static public let whackSound = "whack.caf"
+	static let goodPenguinName = "penguinGood"
+	static let badPenguinName = "penguinBad"
+	static let whackBadSound = "whackBad.caf"
+	static let whackSound = "whack.caf"
+	static let smokeEffectName = "Smoke" // challenge 3
+	static let mudEffectName = "Mud"	// challenge 3
     
     func configure(at position: CGPoint) {
         self.position = position
@@ -38,17 +40,18 @@ class WhackSlot: SKNode {
 		// NOTE: hideTime makes use of popupTime property in the view controller
         if isVisible { return }
 		penguinNode.setScale(1.0)
+		emitMudEffect(moving: .up) // challenge 3
         penguinNode.run(SKAction.moveBy(x: 0, y: 80, duration: 0.1)) // slide a penguin ↑
         isVisible = true
 		isHit = false
         
         // Establish that 1/3 of the time penguin is good, rest of the time it is bad
         if Int.random(in: 0...2) == 0 {
-			penguinNode.texture = SKTexture(imageNamed: WhackSlot.goodPenguinName)
-			penguinNode.name = WhackSlot.goodPenguinName
+			penguinNode.texture = SKTexture(imageNamed: Self.goodPenguinName)
+			penguinNode.name = Self.goodPenguinName
         } else {
-			penguinNode.texture = SKTexture(imageNamed: WhackSlot.badPenguinName)
-			penguinNode.name = WhackSlot.badPenguinName
+			penguinNode.texture = SKTexture(imageNamed: Self.badPenguinName)
+			penguinNode.name = Self.badPenguinName
         }
 		
 		DispatchQueue.main.asyncAfter(deadline: .now() + (hideTime * 3.5)) { [weak self] in
@@ -59,16 +62,68 @@ class WhackSlot: SKNode {
     func hide() {
 		if !(isVisible) { return }
 		penguinNode.run(SKAction.moveBy(x: 0, y: -80, duration: 0.1)) // slide a penguin ↓
+		emitMudEffect(moving: .down) // challenge 3
 		isVisible = false
     }
 	
 	func hit() { // handles the hiding of penguins
 		isHit = true
 		penguinNode.setScale(0.85)
-		
+		emitSmokeEffect() // challenge 3
 		let delay = SKAction.wait(forDuration: 0.25) // wait for a while after hitting
 		let hide = SKAction.moveBy(x: 0, y: -80, duration: 0.5)
 		let notVisible = SKAction.run { [weak self] in self?.isVisible = false }
 		penguinNode.run(SKAction.sequence([delay, hide, notVisible]))
+	}
+}
+
+// challenge 3
+extension WhackSlot {
+	private enum CharacterDirection {
+		case up
+		case down
+	}
+	
+	private func runEffectWithSequence(_ effect: SKEmitterNode) {
+		// this function removes the emitter node after a pre-set time
+		let action	 = SKAction.run { [weak self] in self?.addChild(effect) }
+		let duration = SKAction.wait(forDuration: 2)
+		let removal  = SKAction.run { [weak self] in self?.removeChildren(in: [effect]) }
+		run(SKAction.sequence([action, duration, removal]))
+	}
+	
+	private func emitMudEffect(moving direction: CharacterDirection) {
+		if let mudEffect = SKEmitterNode(fileNamed: Self.mudEffectName) {
+			switch direction {
+			case .up:
+				mudEffect.particleLifetime = 0.30
+				mudEffect.particleSpeed = 1
+				mudEffect.particleSpeedRange = 200
+			case .down:
+				mudEffect.particleLifetime = 0.10
+				mudEffect.particleSpeed = 100
+				mudEffect.particleSpeedRange = 0
+			}
+			mudEffect.position = CGPoint(x: 0, y: 0)
+			mudEffect.zPosition = 1
+			mudEffect.numParticlesToEmit = 100
+			mudEffect.particleBirthRate = 500
+			mudEffect.particleSize = CGSize(width: 30, height: 30)
+			mudEffect.particleColor = SKColor.brown
+			mudEffect.particleBlendMode = .replace
+			runEffectWithSequence(mudEffect)
+		}
+	}
+	
+	fileprivate func emitSmokeEffect() {
+		if let smokeEffect = SKEmitterNode(fileNamed: Self.smokeEffectName) {
+			smokeEffect.position = CGPoint(x: 0, y: 45)
+			smokeEffect.zPosition = 1
+			smokeEffect.numParticlesToEmit = 10
+			smokeEffect.particleLifetime = 0.75
+			smokeEffect.particleColor = SKColor.white
+			smokeEffect.particleAlpha = 0.5
+			runEffectWithSequence(smokeEffect)
+		}
 	}
 }
